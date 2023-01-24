84c17dd4fb86aad6e55bdb4593fc38f6410a5873
aaronjones1 <ajones@cycominc.com>
2023-01-23 23:50:32 -0500

    Add Sign-in button

--- App.tsx
+++ App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useAuth0, Auth0Provider } from 'react-native-auth0';
+
+const SignInButton = () => {
+  const { authorize } = useAuth0();
+
+  const onPress = async () => {
+    try {
+      await authorize();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return <Button onPress={onPress} title='Sign in' />;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Muze</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Auth0Provider
+      domain={'dev-min73mnfgfs8ofzq.us.auth0.com'}
+      clientId={'UZDwRiztDQdJ9wBW1PMXdf3otnq2bcDM'}
+    >
+      <View style={styles.container}>
+        <Text>Muze</Text>
+        <SignInButton />
+        <StatusBar style='auto' />
+      </View>
+    </Auth0Provider>
   );
 }
 
@@ -18,3 +39,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
